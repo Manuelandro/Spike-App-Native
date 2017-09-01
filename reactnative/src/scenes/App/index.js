9229b229c4b65a9d5efcc4b67b20f09d71c278cd
Manuel Palma <manuelpalma@MacBook-Pro-di-Manuel.local>
2017-09-01 18:25:14 +0200

    load assets async

--- reactnative/src/scenes/App/index.js
+++ reactnative/src/scenes/App/index.js
@@ -1,18 +1,43 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
-import { NavigationBar } from '@shoutem/ui'
+import { Font } from 'expo'
+import { NavigationBar, Heading, Icon } from '@shoutem/ui'
 
 import { Wrapper } from './style'
 
-const App = props => (
-    <Wrapper>
-        <NavigationBar
-            leftComponent={<Text>Menu</Text>}
-            centerComponent={<Text>Today</Text>}
-        />
-        <Text>aaa</Text>
-    </Wrapper>
-)
+class App extends React.Component {
+    state = {
+        fontReady: false,
+    }
+
+    async componentWillMount() {
+        await Font.loadAsync({
+            'Rubik-Regular': require('@shoutem/ui/fonts/Rubik-Regular.ttf'),
+            'rubicon-icon-font': require('@shoutem/ui/fonts/rubicon-icon-font.ttf'),
+        })
+
+        this.setState({ fontReady: true })
+
+        await fetch()
+    }
+
+    render() {
+        if (!this.state.fontReady) {
+            return <Text>loading</Text>
+        }
+
+        return (
+            <Wrapper>
+                <NavigationBar
+                    leftComponent={<Icon name="sidebar" />}
+                    centerComponent={<Text>Today</Text>}
+                />
+                <Heading>Heading</Heading>
+                <Text>aaa</Text>
+            </Wrapper>
+        )
+    }
+}
 
 export default App
