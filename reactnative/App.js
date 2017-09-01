e75eb5205cf3d23825b90590d364bb1abf592eb4
Manuel Palma <manuelpalma@MacBook-Pro-di-Manuel.local>
2017-09-01 12:32:19 +0200

    eslint config

--- reactnative/App.js
+++ reactnative/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
+import { Icon, NavigationBar } from '@shoutem/ui'
 
 import configureStore from './src/store'
 
-const store = configureStore()
+const store = {}
 
 export default class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
                 <View style={styles.container}>
+                    <NavigationBar
+                        leftComponent={<Text>Menu</Text>}
+                        centerComponent={<Text>Today</Text>}
+                    />
                     <Text>Open up App.js to start working on your app!</Text>
                     <Text>Changes you make will automatically reloadee.</Text>
                 </View>
