996db287265a5334a3f6b58eba6a0067990306a6
Manuel Palma <manuelpalma@MacBook-Pro-di-Manuel.local>
2017-09-15 13:24:05 +0200

    react-native-navigation

--- today/android/app/src/main/java/com/today/MainApplication.java
+++ today/android/app/src/main/java/com/today/MainApplication.java
@@ -7,11 +7,12 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
